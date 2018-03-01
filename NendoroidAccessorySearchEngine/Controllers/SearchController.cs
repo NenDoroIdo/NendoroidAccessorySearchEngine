c1f49514ac98c3965b5f9313cf42c81d9d75740e
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-03-02 01:06:35 +0900

    Add project files.

---
+++ NendoroidAccessorySearchEngine/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NendoroidAccessorySearchEngine.Controllers
+{
+    /// <summary>
+    /// Search controller
+    /// </summary>
+    public class SearchController : Controller
+    {
+        /// <summary>
+        /// this api is use to upload image,
+        /// and get result 
+        /// return in json type
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
